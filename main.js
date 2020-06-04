f9a63236d996889346a61e267e3643b1477c1b55
Carlos Davila <davila_salazar_carlo@student.smc.edu>
2020-06-04 00:59:04 -0700

    needs more work and time
    i still need to get the contact form to work and fix the picture and add some of my work

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    ///Thanks to https://github.com/mattboldt/typed.js for his typed.js library made it easier
+    var typewriter =new Typed('#typewriter',{
+        strings :['Hi,My name is carlos','And welcome to my page','im still working on it '] ,/// may add something else
+        typeSpeed:40,
+        backSpeed:20,showCursor:true,
+        loop:true
+
+    });
+    function openTab(th)
+    {
+        window.open(th.name, "_self");
+    }
+});
