7846a65c3673a1ee9223cc63afde9674c34e3d8e
shadezs <davila_salazar_carlo@student.smc.edu>
2020-08-31 15:28:50 -0700

    change footer and made minor changes

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-$(document).ready(function () {
+$(document).ready(function() {
     ///Thanks to https://github.com/mattboldt/typed.js for his typed.js library made it easier
-    var typewriter =new Typed('#typewriter',{
-        strings :['Hi,My name is carlos','And welcome to my page','im still working on it '] ,/// may add something else
-        typeSpeed:40,
-        backSpeed:20,showCursor:true,
+    var typewriter = new Typed('#typewriter', {
+        strings: ['Hi :)', 'My name is carlos', 'And welcome to my page', 'im still working on it '], /// may add something else
+        typeSpeed: 40,
+        backSpeed: 20,
+        showCursor: true,
         backDelay: 500,
         cursorChar: '|',
-        loop:false
+        loop: true
 
     });
-    function openTab(th)
-    {
+
+    function openTab(th) {
         window.open(th.name, "_self");
     }
 });
