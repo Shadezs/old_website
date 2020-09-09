ec4d293450c8caa84954831b8f6388ceb1ed9e9a
shadezs <davila_salazar_carlo@student.smc.edu>
2020-09-08 19:58:06 -0700

    change footer, minor style changes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     ///Thanks to https://github.com/mattboldt/typed.js for his typed.js library made it easier
     var typewriter = new Typed('#typewriter', {
-        strings: ['Hi :)', 'My name is carlos', 'And welcome to my page', 'im still working on it '], /// may add something else
+        strings: ['Hi :) My name is carlos', 'And welcome to my page', 'im still working on it '], /// may add something else
         typeSpeed: 40,
         backSpeed: 20,
         showCursor: true,
